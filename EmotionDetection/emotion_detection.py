7f3267845f18142be7fc24bdae31dcc1bccc22e9
Daniel Rockbank <danielrockbank@gmail.com>
2026-03-28 11:56:25 -0700

    ditionary changes to detection.py and 7a edit to show new code structure

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,12 +4,12 @@ import json
 def emotion_detector(text_to_analyze):
     if not text_to_analyze:
         return {
-            "anger: None,"
-            "disgust: None,"
-            "fear: None,"
-            "joy: None,"
-            "sadness: None,"
-            "dominant_emotion: None"
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None,
         }
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -19,12 +19,12 @@ def emotion_detector(text_to_analyze):
 
     if response.status_code == 400:
         return {
-            "anger: None",
-            "disgust: None",
-            "fear: None",
-            "joy: None",
-            "sadness: None",
-            "dominant_emotion: none"
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None,
                 }
     
     response_dict = response.json()
