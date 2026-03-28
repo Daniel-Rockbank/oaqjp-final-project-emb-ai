13d20a0b4a263299eea3f20b748c54d2b2aa0763
Daniel Rockbank <danielrockbank@gmail.com>
2026-03-28 11:32:31 -0700

    added if not statement

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,6 +2,15 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
+    if not text_to_analyze:
+        return {
+            "anger: None,"
+            "disgust: None,"
+            "fear: None,"
+            "joy: None,"
+            "sadness: None,"
+            "dominant_emotion: None"
+        }
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
@@ -9,12 +18,13 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json=input_json, headers=headers)
 
     if response.status_code == 400:
-        return {"anger: None",
-                "disgust: None",
-                "fear: None",
-                "joy: None",
-                "sadness: None",
-                "dominant_emotion: none"
+        return {
+            "anger: None",
+            "disgust: None",
+            "fear: None",
+            "joy: None",
+            "sadness: None",
+            "dominant_emotion: none"
                 }
     
     response_dict = response.json()
